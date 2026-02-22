b5ff4bab34d2d39603d53e04629bbd5f96e26258
Yuri Volkov <volkov.yuri.alex@gmail.com>
2026-02-22 09:42:23 +0100

    Add diff function calculation

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@
 
 pub mod yin;
 
-pub use fixed::types::I24F8;
+pub use fixed::types::I50F14;
 
 /// Default data type
-pub type Sample = I24F8;
+pub type Sample = I50F14;
 
 pub use yin::Yin;
+pub use yin::YinCalculator;
--- src/yin.rs
+++ src/yin.rs
@@ -1,42 +1,55 @@
 
-use core::ops::{Sub, Mul, AddAssign};
+use core::ops::{AddAssign, Div, DivAssign, Mul, Sub};
 
 
 pub struct Yin<T, const N: usize> {
 
-    pub result: [T; N],
+    diff_function: [T; N],
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error{
-    /// Frame size should be 2 * result size
+    /// Frame size should be 2 * diff_function size
     FrameSizeMismatch,
 }
 
 pub trait YinCalculator<T> {
 
-    fn calculate(&mut self, frame: &[T]) -> Result<(), Error>;
+    /// Calculate difference function according to YIN method
+    fn calculate_diff(&mut self, frame: &[T]) -> Result<(), Error>;
 }
 
 impl<T, const N: usize> YinCalculator<T> for Yin<T, N>
-where T: From<u8> + Sub<Output = T> + Mul<Output = T> + AddAssign + Copy{
+where T: From<u8> + Sub<Output = T> + Mul<Output = T> + AddAssign + Copy + Div<Output = T> + DivAssign{
     
-    fn calculate(&mut self, frame: &[T]) -> Result<(), Error>{
+    fn calculate_diff(&mut self, frame: &[T]) -> Result<(), Error>{
         
         if frame.len() != N * 2 {
             return Err(Error::FrameSizeMismatch);
         }
         
-        for t in 0..frame.len()/2 {
+        // Average squared difference over the window of N samples
+        // diff_function(tau)
+        for tau in 0..N {
 
             let mut sum = 0_u8.into();
-            for j in 0..frame.len()/2 {
+            for j in 0..N {
 
-                let diff = frame[j] - frame[j+t];
+                let diff = frame[j] - frame[j+tau];
                 let square = diff * diff;
                 sum += square;
             }
-            self.result[t] = sum;
+            self.diff_function[tau] = sum;
+        }
+        // Cumulative mean normalization
+        let mut sum: T = 0_u8.into();
+        let mut num: T = 0_u8.into();
+        self.diff_function[0] = 1_u8.into();
+        for value in self.diff_function.iter_mut() {
+            sum += *value;
+            num += 1_u8.into();
+            let mean = sum / num;
+            *value /= mean;
         }
 
         Ok(())
@@ -47,6 +60,31 @@ impl<T, const N: usize> Yin<T, N>
 where T: Copy + From<u8> {
 
     pub fn new() -> Self {
-        Yin { result: [1_u8.into(); N] }
+        Yin { diff_function: [1_u8.into(); N] }
+    }
+
+    pub fn diff_function(&self) -> &[T; N]{
+        &self.diff_function
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Sample;
+    use pretty_assertions::{assert_eq, assert_ne};
+    
+
+    #[test]
+    fn correct_frame_size() {
+
+        const FRAME_SIZE: usize = 256;
+        const RES_SIZE: usize = FRAME_SIZE / 2;
+        let frame = [Sample::from_num(0); FRAME_SIZE];
+
+        let mut yin:Yin<Sample, RES_SIZE> = Yin::new();
+
+        assert_eq!(yin.calculate_diff(&frame), Ok(()));
     }
 }
